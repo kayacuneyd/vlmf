6e318d45d6067686f1af7efaa6b068fd698d98d8
Cüneyt Kaya <kayacuneyd@gmail.com>
2025-05-14 23:41:06 +0200

    ilk yediiiiiiiii deneme

--- template-parts/frontpage/hero.php
+++ template-parts/frontpage/hero.php
@@ -6,27 +6,25 @@
         <div class="row align-items-center">
             <div class="col-lg-6 col-md-12 col-12">
                 <div class="header-content">
-                    <h1>Corporate & Business Site Template by Ayro UI.</h1>
-                    <p>
-                        We are a digital agency that helps brands to achieve their
-                        business outcomes. We see technology as a tool to create amazing
-                        things.
-                    </p>
+                    <h1><?php the_field('hero_title'); ?></h1>
+                    <p><?php the_field('hero_desc'); ?></p>
                     <div class="button">
-                        <a href="javascript:void(0)" class="btn primary-btn">Get Started</a>
-                        <a href="https://www.youtube.com/watch?v=r44RKWyfcFw&fbclid=IwAR21beSJORalzmzokxDRcGfkZA1AtRTE__l5N4r09HcGS5Y6vOluyouM9EM"
-                            class="glightbox video-button">
+                        <a href="<?php the_field('hero_button_link'); ?>" class="btn primary-btn"><?php the_field('hero_button_text'); ?></a>
+                        <a href="<?php the_field('hero_video_link'); ?>" class="glightbox video-button">
                             <span class="btn icon-btn rounded-full">
                                 <i class="lni lni-play"></i>
                             </span>
-                            <span class="text">Watch Intro</span>
+                            <span class="text"><?php the_field('hero_video_label'); ?></span>
                         </a>
                     </div>
                 </div>
             </div>
             <div class="col-lg-6 col-md-12 col-12">
                 <div class="header-image">
-                    <img src="assets/images/header/hero-image.jpg" alt="#" />
+                    <?php $hero_img = get_field('hero_image'); ?>
+                    <?php if ($hero_img): ?>
+                        <img src="<?php echo esc_url($hero_img['url']); ?>" alt="<?php echo esc_attr($hero_img['alt']); ?>" />
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
