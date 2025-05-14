f3f25c6d86f358e1ebcd0cd7cbcc40ac3a84a61d
Cüneyt Kaya <kayacuneyd@gmail.com>
2025-05-15 00:22:35 +0200

    ilk sekizzzzz deneme

--- front-page.php
+++ front-page.php
@@ -7,7 +7,7 @@ get_header();
 <?php get_template_part('template-parts/frontpage/services'); ?>
 <!--?php get_template_part('template-parts/frontpage/team'); ?-->
 <?php get_template_part('template-parts/frontpage/cta'); ?>
-<?php get_template_part('template-parts/frontpage/blog'); ?>
+<!--?php get_template_part('template-parts/frontpage/blog'); ?-->
 <?php get_template_part('template-parts/frontpage/clients'); ?>
 <?php get_template_part('template-parts/frontpage/contact'); ?>
 <?php get_template_part('template-parts/frontpage/map'); ?>
