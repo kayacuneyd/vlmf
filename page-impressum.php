3fae4f0caa0f44a23fb2f09eee7064c36453d32e
Cüneyt Kaya <kayacuneyd@gmail.com>
2025-05-23 00:17:03 +0200

    impressum last version

---
+++ page-impressum.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Impressum
+
+*/
+get_header(); ?>
+<section class="daten-eight">
+    <article class="page-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-12">
+                    <div class="content-area">
+                        <?php the_content(); ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
+
+
+
+
+<?php get_footer(); ?>
