d5b2b608437fb133f2ea9f6000ec916821b61276
Cüneyt Kaya <kayacuneyd@gmail.com>
2025-05-23 00:18:52 +0200

    impressum last versions

--- page-datenschutz.php
+++ page-datenschutz.php
@@ -8,6 +8,7 @@ get_header(); ?>
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-12">
+                    <h1><?php the_title(); ?></h1>
                     <div class="content-area">
                         <?php the_content(); ?>
 
--- page-impressum.php
+++ page-impressum.php
@@ -9,9 +9,9 @@ get_header(); ?>
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-12">
+                    <h1><?php the_title(); ?></h1>
                     <div class="content-area">
                         <?php the_content(); ?>
-
                     </div>
                 </div>
             </div>
